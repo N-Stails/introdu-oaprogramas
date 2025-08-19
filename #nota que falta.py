ebf9e5642869cb7408b1e24630d0e82485d814be
N-Stails <nicollas.s@estudante.ifmt.edu.br>
2025-08-19 10:11:04 -0300

    Add files via upload

---
+++ #nota que falta.py
@@ -0,0 +1,7 @@
+#nota que falta
+n1 = float(input("insira a sua nota do primeiro bimestre:"))
+n2 = float(input("insira a sua nota do primeiro bimestre:"))
+n3 = (60 - ((n1*2)+(n2*2)))/2
+n4 = (60 - ((n1*2)+(n2*2)))/2
+print("você precisa tirar, no terceiro bimestre:",n3/3)
+print("você precisa tirar, no quarto bimestre:",n4/3)
