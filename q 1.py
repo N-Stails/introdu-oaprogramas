42efa2e52b1c4b0bb838075b983380957feb83b5
N-Stails <nicollas.s@estudante.ifmt.edu.br>
2025-11-05 15:52:52 -0300

    prova 2 1

--- q 1.py
+++ q 1.py
@@ -1,11 +1,9 @@
 #Arthur, Erick, Nicollas
 
 n = int(input("insira seu número: "))
-p = "resultado = "
-for i in range (1, n+1):
-    str(i)
-    p += f"{i}!"
-    if i != n:
-        p += " + "
+s = 0
+while n != 0:
+    s += n%10
+    n = n//10
 
-print(f"a soma dos fatoriais de todos os números entre 1 e esse número é: {p}")
+print(f"a soma dos algorismos inseridos é: {s}")
