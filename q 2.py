12644a36de15a3b34e3274d5d260e5e9f7f9115e
N-Stails <nicollas.s@estudante.ifmt.edu.br>
2025-11-05 15:43:31 -0300

    prova 5

--- q 2.py
+++ q 2.py
@@ -1,43 +1,8 @@
 #Arthur, Erick, Nicollas
 
-n = int(input("insira seu número: "))
-f = 0
-while n != 0:
-    v = n%16
-    if v == 0:
-        f += v
-    if v == 1:
-        f += v
-    if v == 2:
-        f += v
-    if v == 3:
-        f += v
-    if v == 4:
-        f += v
-    if v == 5:
-        f += v
-    if v == 6:
-        f += v
-    if v == 7:
-        f += v
-    if v == 8:
-        f += v
-    if v == 9:
-        f += v
-    if v == 10:
-        f += v
-    if v == 11:
-        f += v
-    if v == 12:
-        f += v
-    if v == 13:
-        f += v
-    if v == 14:
-        f += v
-    if v == 15:
-        f += v
-    if v == 16:
-        f += v
-    n //= 16
+s = 0
+for i in range (1, 201):
+    if i % 3 == 0:
+        s += i
 
-print(f"a conversão de decimal para hexadecimal de seu número é: {f}")
+print(f"a soma de todos os múltiplos de 3 entre 1 e 200 é: {s}")
